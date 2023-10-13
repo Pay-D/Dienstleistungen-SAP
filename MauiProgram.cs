96118d254843db5d6dbe1edc62469cbcd3d1ef58
Pay-D <Patrick.reuter224b@gmail.com>
2023-10-13 16:06:04 +0200

    feat: add community-toolkit

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
 
 namespace Dienstleistungen_SAP
 {
@@ -9,6 +10,7 @@ namespace Dienstleistungen_SAP
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
