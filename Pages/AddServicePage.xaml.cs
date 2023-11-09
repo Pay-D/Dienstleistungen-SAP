b31b2f5737a349258c070c0420f3ef195eaf904e
Pay-D <Patrick.reuter224b@gmail.com>
2023-11-09 15:23:07 +0100

    feat: add type in service creation

--- Pages/AddServicePage.xaml.cs
+++ Pages/AddServicePage.xaml.cs
@@ -1,3 +1,4 @@
+using Dienstleistungen_SAP.DataModels;
 using Dienstleistungen_SAP.ViewModel;
 
 namespace Dienstleistungen_SAP.Pages;
@@ -8,5 +9,7 @@ public partial class AddServicePage : ContentPage
 	{
 		InitializeComponent();
 		BindingContext = vm;
-	}
+		offerRadioButton.Value = Service.ServiceType.Offer;
+        requestRadioButton.Value = Service.ServiceType.Request;
+    }
 }
